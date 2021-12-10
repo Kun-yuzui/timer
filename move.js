4a5878a908e32f82b9385ad8c710c63032c34eba
kun <82259944+Kun-yuzui@users.noreply.github.com>
2021-12-11 00:40:41 +0800

    宽高改写
    当鼠标移到元素上时，一个元素的宽发生改变，一个元素的高发生改变；
    之前使用的是两个参数，但在这里会产生冗余，因此要再增加一个参数，也是说这时候已经有三个参数了；
    产生三个参数同时，会得到一个共用的公式，将该 JS 公式存储在一个 JS 文件中，即 move.js 。

---
+++ move.js
@@ -0,0 +1,12 @@
+function move(obj, name, target) {
+	clearInterval(obj.timerA);
+	obj.timerA = setInterval(function() {
+		let nowStyle = parseFloat(getComputedStyle(obj)[name]);
+		if (nowStyle == target) {
+			clearInterval(obj.timerA);
+		} else {
+			let speed = (target - nowStyle) / 10;
+			obj.style[name] = nowStyle + speed + 'px';
+		}
+	}, 30)
+}
